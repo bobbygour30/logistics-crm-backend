27a75e0d8e460e0432272f149a23878c6ecc0bdb
bobbygour30 <bobby30gour@gmail.com>
2026-01-03 14:26:30 +0530

    updated

--- index.js
+++ index.js
@@ -422,8 +422,6 @@ app.use('*', (req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
-})
+
 // Export the app for Vercel serverless
 module.exports = app;
